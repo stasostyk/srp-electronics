80ec6c12ae6608b2a78003abe04df6eb0d913c19
Staś Ostyk <s.ostyks@gmail.com>
2023-04-21 15:12:47 +0200

    pressure calculations

--- code.py
+++ code.py
@@ -5,12 +5,23 @@ import measure
 import statemachine
 import buzzer
 import config
+import numpy as np
+
+# physics constants
+M_earth = 0.0289644
+GRAV = 9.80665
+R = 8.31432
+LAPSE_RATE = 0.0065
+
 
 delay_pyro_miliseconds = config.get_deployment_timer()
 print(f'Read in a delay of {delay_pyro_miliseconds} ms from the config file')
 
 states = statemachine.Statemachine(PYRO_FIRE_DELAY_MS = delay_pyro_miliseconds)
 
+def pressureToAltitude(pressure):
+    return (temp_0 / LAPSE_RATE) * ((pressure / pressure_0) ** (-R * LAPSE_RATE / GRAV * M_earth) - 1)
+
 while True:
-   buzzer.buzzer_tick()
-   states.tick()
+    buzzer.buzzer_tick()
+    states.tick()
