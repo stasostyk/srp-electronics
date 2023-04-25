015c369ba57fdb2a13db30c5615fe7d667b7ca24
Staś Ostyk <s.ostyks@gmail.com>
2023-04-25 12:56:31 +0200

    clean up constants

--- parachute.py
+++ parachute.py
@@ -1,12 +1,5 @@
 import numpy as np
 
-# physics constants
-M_earth = 0.0289644
-GRAV = 9.80665
-R = 8.31432
-LAPSE_RATE = 0.0065
-
-
 def detectApogee(pressure_log, time_log):
     slope, intercept = np.polyfit(time_log, pressure_log, 1)
 
