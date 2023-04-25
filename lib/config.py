6975100f123241532029ad3dde37424957fc86c1
Staś Ostyk <s.ostyks@gmail.com>
2023-04-25 11:31:04 +0200

    read configs

--- lib/config.py
+++ lib/config.py
@@ -1,12 +1,13 @@
 import json
 
-def get_deployment_timer():
-    '''
-    Very simple method that reads a certain key from a JSON file that is stored on the feather
-    in this case it is looking for the "deployment_delay_in_miliseconds" key
-    There is no exception catching as the board should crash out when the config is not complete
-    '''
+def get_apogee():
     parsed_jsondata = json.loads(open('config.json', 'r').read())
-    delay = parsed_jsondata['deployment_delay_in_miliseconds']
+    return parsed_jsondata['apogee']
 
-    return delay
+def get_window_before():
+    parsed_jsondata = json.loads(open('config.json', 'r').read())
+    return parsed_jsondata['window_before']
+
+def get_window_after():
+    parsed_jsondata = json.loads(open('config.json', 'r').read())
+    return parsed_jsondata['window_after']
